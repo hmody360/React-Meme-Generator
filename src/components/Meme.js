5eb8e22dcecc1fb72c4fb893307978ebb9d5c961
hmody360 <s441010207@st.uqu.edu.sa>
2023-12-05 00:46:24 +0300

    Feature: Made meme generation as a seperate component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,26 @@
 import React from "react";
+import memeData from "../memedata"
 
-import memeData from "../memeData"
-let memeImage
 const Meme = () => {
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        memeImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeData, setAllMemeData] = React.useState(memeData.data.memes)
     const randomMeme = () => {
-        const memeElems = memeData.data.memes.map( elem => elem.url)
+        const memeElems = allMemeData.map( elem => elem.url)
         
         let RandomImage = memeElems[Math.floor(Math.random() * memeElems.length)]
         console.log(RandomImage);
-        document.querySelector(".meme-image").src = RandomImage //This is the original way to do it in JS, but not React has better ways
+        // document.querySelector(".meme-image").src = RandomImage //This is the original way to do it in JS, but not React has better ways
+        setMeme(meme => {
+            return ({
+                ...meme,
+                memeImage: RandomImage
+            })
+        })
     }
     return (
         <div className="meme-container">
@@ -18,7 +30,7 @@ const Meme = () => {
             </div>
             <button className="generate-btn" onClick={randomMeme}>Generate new meme image<i className="fa-solid fa-image" id="image-icon"></i>
             </button>
-            <img className="meme-image" />
+            <img className="meme-image" src={meme.memeImage}/>
         </div>
     )
 }
