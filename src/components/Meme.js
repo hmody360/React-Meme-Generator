c4eece39e7445f0ac7335fda04c004a5276ef3ad
hmody360 <s441010207@st.uqu.edu.sa>
2023-12-06 20:45:21 +0300

    Feature: Added dynamic top and bottom text writing, and meme generation through the API

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,4 @@
 import React from "react";
-import memeData from "../memedata"
 
 const Meme = () => {
     const [meme, setMeme] = React.useState({
@@ -8,10 +7,17 @@ const Meme = () => {
         memeImage: "http://i.imgflip.com/1bij.jpg"
     })
 
-    const [allMemeData, setAllMemeData] = React.useState(memeData.data.memes)
+    const [allMemes, setAllMemes] = React.useState([])
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name] : value
+        }))
+    }
     const randomMeme = () => {
-        const memeElems = allMemeData.map( elem => elem.url)
-        
+        const memeElems = allMemes.map(elem => elem.url)
+
         let RandomImage = memeElems[Math.floor(Math.random() * memeElems.length)]
         console.log(RandomImage);
         // document.querySelector(".meme-image").src = RandomImage //This is the original way to do it in JS, but not React has better ways
@@ -22,15 +28,25 @@ const Meme = () => {
             })
         })
     }
+        React.useEffect(() => {
+            fetch("https://api.imgflip.com/get_memes")
+            .then(memes => memes.json())
+            .then(data => setAllMemes(data.data.memes))
+        }, [])   
+    
     return (
         <div className="meme-container">
             <div className="input-container">
-                <input type="text" placeholder="Insert top part" className="meme-input" id="top-input"></input>
-                <input type="text" placeholder="Insert bottom part" className="meme-input" id="bottom-input"></input>
+                <input type="text" placeholder="Insert top part" className="meme-input" id="top-input" name="topText" onChange={handleChange} value={meme.topText}></input>
+                <input type="text" placeholder="Insert bottom part" className="meme-input" id="bottom-input" name="bottomText" onChange={handleChange} value={meme.bottomText}></input>
             </div>
             <button className="generate-btn" onClick={randomMeme}>Generate new meme image<i className="fa-solid fa-image" id="image-icon"></i>
             </button>
-            <img className="meme-image" src={meme.memeImage}/>
+            <div className="meme">
+                <img className="meme-image" src={meme.memeImage} />
+                <h2 className="meme-top-text">{meme.topText}</h2>
+                <h2 className="meme-bottom-text">{meme.bottomText}</h2>
+            </div>
         </div>
     )
 }
